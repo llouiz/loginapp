08820737ac9bb1b86eb9405651641e14c90bd5fa
Luiz Carlos <luiz.carlos.techie@gmail.com>
2021-09-15 21:39:18 -0300

    imports

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
